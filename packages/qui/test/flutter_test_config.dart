d5b502a55a8378dd61c23864287f2fc604a95cd3
Ryan Holanda <ryanholanda08@gmail.com>
2026-06-07 12:59:35 -0300

    chore: show golden fonts in macos

--- packages/qui/test/flutter_test_config.dart
+++ packages/qui/test/flutter_test_config.dart
@@ -2,17 +2,33 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:alchemist/alchemist.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:qui/gen/fonts.gen.dart';
 import 'package:qui/src/theme/qui_theme.dart';
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
   final isRunningInCi = Platform.environment['CI'] == 'true';
+  await _loadQuiFonts();
 
   return AlchemistConfig.runWithConfig(
     config: AlchemistConfig(
       theme: QuiTheme.light(primaryColor: const Color(0xFFFF4A4B)),
-      platformGoldensConfig: PlatformGoldensConfig(enabled: !isRunningInCi),
+      platformGoldensConfig: PlatformGoldensConfig(
+        enabled: !isRunningInCi,
+        theme: QuiTheme.light(primaryColor: const Color(0xFFFF4A4B)),
+      ),
     ),
     run: testMain,
   );
+}
+
+Future<void> _loadQuiFonts() async {
+  final interLoader = FontLoader(FontFamily.inter)
+    ..addFont(rootBundle.load('packages/qui/assets/fonts/inter_variable.ttf'))
+    ..addFont(rootBundle.load('packages/qui/assets/fonts/inter_italic.ttf'));
+
+  await interLoader.load();
 }
