affd79cf2cccf67821c12f1abb94c7cbec76fbc4
Ryan Holanda <ryanholanda08@gmail.com>
2026-06-19 18:07:42 -0300

    feat: create qui secondary button

---
+++ packages/qui/lib/src/widgets/qui_secondary_button_enums.dart
@@ -0,0 +1,10 @@
+part of 'qui_secondary_button.dart';
+
+/// Controls the width sizing behavior of [QuiSecondaryButton].
+enum QuiSecondaryButtonFit {
+  /// The button shrink-wraps to its content.
+  fit,
+
+  /// The button expands to fill the available horizontal width.
+  expand,
+}
