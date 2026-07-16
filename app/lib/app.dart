20a46ab995c90ecd273aedf1825528e0c2f32dad
Ryan Holanda <ryanholanda08@gmail.com>
2026-07-15 21:24:57 -0300

    feat(app): use qui app router

--- app/lib/app.dart
+++ app/lib/app.dart
@@ -2,7 +2,6 @@ import 'package:cataqui_app/core/providers.dart';
 import 'package:cataqui_app/i18n/strings.g.dart';
 import 'package:cataqui_app/widgets/app_animated_splash/app_animated_splash.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:qui/qui.dart';
@@ -15,27 +14,16 @@ class CataquiApp extends ConsumerWidget {
     final goRouter = ref.watch(goRouterProvider);
     final i18n = ref.watch(translationProvider);
 
-    return AnnotatedRegion<SystemUiOverlayStyle>(
-      value: const SystemUiOverlayStyle(
-        statusBarColor: Colors.transparent,
-        statusBarIconBrightness: Brightness.dark,
-        statusBarBrightness: Brightness.light,
-        systemNavigationBarColor: Colors.transparent,
-        systemNavigationBarDividerColor: Colors.transparent,
-        systemNavigationBarIconBrightness: Brightness.dark,
-        systemNavigationBarContrastEnforced: false,
-        systemStatusBarContrastEnforced: false,
-      ),
-      child: MaterialApp.router(
-        title: i18n.app.name,
-        routerConfig: goRouter,
-        theme: QuiTheme.light(),
-        locale: i18n.$meta.locale.flutterLocale,
-        supportedLocales: AppLocaleUtils.supportedLocales,
-        localizationsDelegates: GlobalMaterialLocalizations.delegates,
-        debugShowCheckedModeBanner: false,
-        builder: (context, child) => AppAnimatedSplash(child: child ?? const SizedBox.shrink()),
-      ),
+    return QuiApp.router(
+      title: i18n.app.name,
+      color: (primary: const Color(0xFFFF4A4B), onPrimary: const Color(0xFFFFFFFF)),
+      routerConfig: goRouter,
+      locale: i18n.$meta.locale.flutterLocale,
+      supportedLocales: AppLocaleUtils.supportedLocales,
+      localizationsDelegates: GlobalMaterialLocalizations.delegates,
+      builder: (context, child) {
+        return AppAnimatedSplash(child: child ?? const SizedBox.shrink());
+      },
     );
   }
 }
